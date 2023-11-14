44334d0086015e2ef5f631851498d56389254921
kid <kidiatoliny@akira-io.com>
2023-11-14 18:21:07 -0100

    chore: add pt language support

---
+++ resources/lang/pt/form.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'fields' => [
+        'amount' => 'Valor',
+        'arrival_time' => 'Hora de Chegada',
+        'client' => 'Cliente',
+        'code' => 'Código',
+        'country' => 'País',
+        'departure_time' => 'Hora de Partida',
+        'expenses_type' => 'Tipo de Despesa',
+        'fare_type' => 'Tipo de Tarifa',
+        'name' => 'Nome',
+        'nif' => 'NIF',
+        'price' => 'Preço',
+        'role' => 'Role',
+        'route' => 'Rota',
+        'time' => 'Hora',
+        'contacts' => 'Contactos',
+    ],
+
+    'actions' => [
+        'add_contact' => 'Adicionar Contacto',
+    ],
+
+    'time' => [
+        'helper_text' => 'Adicionar hora no formato HH:MM (e.g. 13:00)',
+    ],
+
+];
